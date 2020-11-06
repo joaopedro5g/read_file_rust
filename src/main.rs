277de463b762c30a5b8a4afe3b74af2d49f6130d
joaopedro5g <ebtjoaopedro@gmail.com>
2020-11-05 23:22:18 -0300

    Rust Read File

---
+++ src/main.rs
@@ -0,0 +1,11 @@
+use std::fs::File;
+use std::io::prelude::*;
+
+fn main() {
+  let mut file = File::open("info.txt").expect("Can't open file");
+
+  let mut contents = String::new();
+  file.read_to_string(&mut contents).expect("Ops...");
+
+  println!("File Contents:\n\n {}", contents);
+}
